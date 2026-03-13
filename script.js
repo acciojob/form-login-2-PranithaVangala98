edddb756b1143f62288e94a4810d0b3c6bfb6070
PranithaVangala98 <108581187+PranithaVangala98@users.noreply.github.com>
2026-03-13 15:34:02 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
 //your JS code here. If required.
+function showAlert(){
+  const firstName = document.getElementById("firstName").value;
+  const lastName = document.getElementById("lastName").value;
+  const phone = document.getElementById("phoneNumber").value;
+  const email = document.getElementById("email").value;
+
+  alert(
+    "First Name: " + firstName + " " +
+    "Last Name: " + lastName + " " +
+    "Phone Number: " + phone + " " +
+    "Email ID: " + email
+  );
+}
